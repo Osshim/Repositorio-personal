b66de8ce079c4b8e7702abf323db56348b088c2e
Osshim <carlos.vilchez.t@uni.pe>
2023-02-06 20:50:30 -0500

    se agrego el sout Hols doy un commit GA

--- src/main/java/Pruebas.java
+++ src/main/java/Pruebas.java
@@ -2,5 +2,6 @@ public class Pruebas {
     public static void main(String[] args)
     {
         System.out.println("hola mundo");
+        System.out.printf("Hola soy un nuevo commit GAAAAAAAAAAAAAAAAAAAAA");
     }
 }
